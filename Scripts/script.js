609f669b0ed9526ef538b36d46d43f90265608c8
tansin-nabil <tansin.nabil@northsouth.edu>
2023-02-15 21:59:34 +0600

    styling complete

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,4 @@
 document.getElementById("loginButton").addEventListener("click", function(){
     document.getElementById("login-area").style.display="none";
+    document.getElementById("transaction-area").style.display="block";
 });
